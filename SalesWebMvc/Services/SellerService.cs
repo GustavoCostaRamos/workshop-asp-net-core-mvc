e3237f02a90f41a92184fc0c3bc7b8925d29dd83
GustavoCostaRamos <gustavocosta950@outlook.com>
2022-11-13 02:00:34 -0300

    Delete seller

--- SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/Services/SellerService.cs
@@ -13,7 +13,7 @@ namespace SalesWebMvc.Services
         public SellerService(SalesWebMvcContext context)
         {
             _context = context;
-        } 
+        }
 
         public List<Seller> FindAll()
         {
@@ -23,6 +23,18 @@ namespace SalesWebMvc.Services
         public void Insert(Seller obj)
         {
             _context.Add(obj);
+            _context.SaveChanges();
+        }
+
+        public Seller FindById(int id)
+        {
+            return _context.Seller.FirstOrDefault(obj => obj.Id == id);
+        }
+
+        public void Remove(int id)
+        {
+            var obj = _context.Seller.Find(id);
+            _context.Seller.Remove(obj);
             _context.SaveChanges();
         }
     }
